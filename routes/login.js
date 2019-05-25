c9c004e6c6f37c48bd1603c6734a264e0439c3f4
knoxgon <volkan.guven@hotmail.se>
2019-05-25 13:50:25 +0200

    Concealed exception message due security

--- routes/login.js
+++ routes/login.js
@@ -28,7 +28,7 @@ router.post('/login', async (req, res) => {
         }
         res.send({message: "Success"});
     }catch (e) {
-        res.send({message: e.message});
+        res.send({message: "Internal Server Error"});
     }
 });
 
