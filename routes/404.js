27eaaa2bf034b0006413457a3a1592920e9baa4e
knoxgon <volkan.guven@hotmail.se>
2019-05-25 14:36:31 +0200

    Files deletion completed

--- routes/404.js
+++ routes/404.js
@@ -5,4 +5,8 @@ route.get('/', (req, res, next) => {
     res.json({code: 404, reason: 'Page not valid'});
 });
 
+route.post('/', (req, res, next) => {
+    res.json({code: 404, reason: 'Page not valid'});
+});
+
 module.exports = route;
