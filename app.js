0bba49911ad2e993bfaae61021b99520edf7ffdf
knoxgon <volkan.guven@hotmail.se>
2019-05-25 14:32:50 +0200

    Removed unnecessary files

--- app.js
+++ app.js
@@ -20,15 +20,10 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(indexRouter);
 app.use(usersRouter);
