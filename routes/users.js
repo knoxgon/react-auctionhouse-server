330068efa832e2b2f6c2177d04bda4a7dc9d6530
knoxgon <volkan.guven@hotmail.se>
2019-05-25 13:47:09 +0200

    Moved user registration router to its own file

--- routes/users.js
+++ routes/users.js
@@ -7,32 +7,14 @@ const bcrypt = require('bcrypt');
 
 const excludeFields = {password: 0, _id: 0, __v: 0};
 
-//Create user based on Joi Validator
-router.post('/users', (req, res) => {
-    req.body.password = bcrypt.hashSync(req.body.password, 10);
-    let user = new userModel(req.body);
-    let userResult = user.joiValidate(req.body);
-    if(userResult[0]){
-        user.save().then(item => {
-            res.sendStatus(201);
-        }).catch((err) => res.status(400).send(err.errmsg));
-    } else {
-        let arrs = userResult[1].message.split('. ');
-        let newList = [];
-        for(var i = 0; i < arrs.length; i++){
-            newList.push(JSON.stringify(
-                {errorMessage : arrs[i]})
-                .replace(/[\\'"]+/g, ''));
-        }
-        res.status(400).send(newList);
-    }
-});
+///////GET all///////
 
 router.get('/users', (req, res) => {
 /*We want to extract everything but password, id, and version*/
     userModel.find({}, excludeFields, (err, users) => res.send(users));
 });
 
+///////GET specific///////
 
 //Find user based on the username;
 router.get('/users/:username', (req, res) => {
@@ -42,6 +24,5 @@ router.get('/users/:username', (req, res) => {
    );
 });
 
-//Change password
 
 module.exports = router;
