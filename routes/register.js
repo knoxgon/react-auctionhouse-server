26992aa366011e6e989735a11d8723fda6e35098
knoxgon <volkan.guven@hotmail.se>
2019-05-25 14:18:47 +0200

    Added password and username validators

--- routes/register.js
+++ routes/register.js
@@ -7,22 +7,35 @@ const bcrypt = require('bcrypt');
 
 //Create user based on Joi Validator
 router.post('/register', (req, res) => {
-    req.body.password = bcrypt.hashSync(req.body.password, 10);
-    let user = new userModel(req.body);
-    let userResult = user.joiValidate(req.body);
-    if(userResult[0]){
-        user.save().then(item => {
-            res.sendStatus(201);
-        }).catch((err) => res.status(400).send(err.errmsg));
-    } else {
-        let arrs = userResult[1].message.split('. ');
-        let newList = [];
-        for(var i = 0; i < arrs.length; i++){
-            newList.push(JSON.stringify(
-                {errorMessage : arrs[i]})
-                .replace(/[\\'"]+/g, ''));
+    //Validate username
+    let invalidUsername = new RegExp(/[~`!#$%^()€¤äåö&*+@=\-\[\]\\';,/{}|":<>?]/)
+                            .test(req.body.username);
+    //Validate password
+    if(req.body.password.length < 8) {
+        res.status(400).send({message: 'Password length must be 8 or more characters'})
+        return;
+    }
+    if(!invalidUsername) {
+        req.body.password = bcrypt.hashSync(req.body.password, 10);
+        let user = new userModel(req.body);
+        let userResult = user.joiValidate(req.body);
+        if (userResult[0]) {
+            user.save().then(item => {
+                res.sendStatus(201);
+            }).catch((err) => res.status(400).send(err.errmsg));
+        } else {
+            //Send error list
+            let arrs = userResult[1].message.split('. ');
+            let newList = [];
+            for (var i = 0; i < arrs.length; i++) {
+                newList.push(JSON.stringify(
+                    {errorMessage: arrs[i]})
+                    .replace(/[\\'"]+/g, ''));
+            }
+            res.status(400).send(newList);
         }
-        res.status(400).send(newList);
+    } else {
+        res.status(400).send({message: 'username cannot contain special characters'});
     }
 });
 
