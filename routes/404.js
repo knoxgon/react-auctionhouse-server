1996f78826a190c5f082ad5895440c11b5ff82c8
knoxgon <volkan.guven@hotmail.se>
2019-05-28 17:24:04 +0200

    Added 404 invalid page routes

--- routes/404.js
+++ routes/404.js
@@ -2,11 +2,18 @@ var express = require('express');
 var route = express.Router();
 
 route.get('/', (req, res, next) => {
-    res.json({code: 404, reason: 'Page not valid'});
+    res.json({ code: 404, reason: 'Page not found' });
+    next();
 });
 
 route.post('/', (req, res, next) => {
-    res.json({code: 404, reason: 'Page not valid'});
+    res.json({ code: 404, reason: 'Page not found' });
+    next();
+});
+
+route.delete('/', (req, res, next) => {
+    res.json({ code: 404, reason: 'Page not found' });
+    next();
 });
 
 module.exports = route;
