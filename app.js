d1c7f4d59755d09b813d790e639e57d4321b15dd
knoxgon <volkan.guven@hotmail.se>
2019-05-28 17:25:09 +0200

    Added CORS policy header parameters for localhost front-to-backend connectivity

--- app.js
+++ app.js
@@ -2,22 +2,21 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var uuid = require('uuid');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var registerRouter = require('./routes/register');
-var notFoundRoute  = require('./routes/404');
+var notFoundRoute = require('./routes/404');
+var bodyParser = require('body-parser');
 
 var app = express();
 
-var bodyParser = require('body-parser');
 app.use(bodyParser.json({
   strict: false
 }));
+
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
@@ -28,13 +27,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(session({
-  genid: uuid.v4,
-  secret: 'myTempSecKey',
-  resave: false,
-  saveUninitialized: true
-}));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(indexRouter);
 app.use(usersRouter);
@@ -43,13 +41,13 @@ app.use(registerRouter);
 app.use('**', notFoundRoute);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
