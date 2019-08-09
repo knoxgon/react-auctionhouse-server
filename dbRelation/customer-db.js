8a3b9160c255f4683a73e6fa2f51dfb8f8280b19
knoxgon <volkan.guven@hotmail.se>
2019-08-09 09:54:28 +0200

    Schedule db links added to customer-db

--- dbRelation/customer-db.js
+++ dbRelation/customer-db.js
@@ -7,6 +7,8 @@ const raw = JSON.parse(data);
 exports.custUrl = {
     "s_db": raw.customer[0].s_db,
     "s_user": raw.customer[1].s_user,
+    "s_schedule": raw.customer[2].s_schedule,
     "db": raw.customer[0].db,
-    "user": raw.customer[1].user
+    "user": raw.customer[1].user,
+    "schedule": raw.customer[2].schedule
 };
