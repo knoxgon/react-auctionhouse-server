da7bbbfa216638eaf7ea21b39e163c784d6c01fa
knoxgon <volkan.guven@hotmail.se>
2019-08-15 12:06:04 +0200

    Added controllers for the users

---
+++ controllers/user-controller.js
@@ -0,0 +1,28 @@
+let service = require('../services/user-service')
+
+module.exports.fetchUsers = async function (req, res, next) {
+  await service.getUsers()
+    .then((result) => {
+      res.send(result);
+    }).catch((err) => {
+      res.send(err)
+    })
+}
+
+module.exports.fetchOneUser = async function (req, res, next) {
+  await service.findUser(req.params.username)
+    .then((result) => {
+      res.send(result);
+    }).catch((err) => {
+      res.status(404).send(err)
+    })
+}
+
+module.exports.removeOneUser = async function (req, res, next) {
+  await service.delUser(req.params.username)
+    .then((result) => {
+      res.send(result);
+    }).catch((err) => {
+      res.status(404).send(err)
+    })
+}
