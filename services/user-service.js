cc1fcd27be03d92be5b1c0adf0dfb88d18281a87
knoxgon <volkan.guven@hotmail.se>
2019-08-15 12:03:32 +0200

    Added user services

---
+++ services/user-service.js
@@ -0,0 +1,42 @@
+const excludeFields = { password: 0, _id: 0, __v: 0 };
+const userModel = require('../models/user-model');
+
+//We want to extract everything but password, id, and version
+module.exports.getUsers = async function () {
+  return new Promise((resolve, reject) => {
+    userModel.find({}, excludeFields, (err, result) => {
+      if (err) return reject(err)
+      return resolve(result)
+    })
+  })
+}
+
+//Find user based on the username or email
+module.exports.findUser = async function (param) {
+  return new Promise((resolve, reject) => {
+    userModel.findOne(
+      { $or: [{ email: param }, { username: param }] },
+      excludeFields, (err, fin) => {
+        if (!fin) reject({ "failure": "User not exists." });
+        return resolve(fin);
+      }
+    )
+  });
+}
+
+//Delete user based on username or email
+module.exports.delUser = async function (param) {
+  return new Promise((resolve, reject) => {
+    userModel.deleteOne({ $or: [{ username: param }, { email: param }] },
+      (err, user) => {
+        if (!err) {
+          if (user.n == 0 && user.ok == 1 && user.deletedCount == 0) {
+            reject({ message: 'No such user.' });
+          } else {
+            resolve({ message: 'Success' });
+          }
+        } else
+          reject('Internal Error');
+      });
+  })
+}
