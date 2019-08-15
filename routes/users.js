21770b612113422499464aef60316106dee7cf24
knoxgon <volkan.guven@hotmail.se>
2019-08-15 12:04:32 +0200

    Added user service functions

--- routes/users.js
+++ routes/users.js
@@ -1,49 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const userModel = require('../models/user-model');
-const excludeFields = { password: 0, _id: 0, __v: 0 };
-
 const asyncmw = require('../utils/async-middleware');
-
 let verifier = require('../auth/jwtVerifier');
+let userCtrl = require('../controllers/user-controller');
 
-/*We want to extract everything but password, id, and version*/
-router.get('/users', verifier, asyncmw(async (req, res, next) => {
-    await userModel.find({}, excludeFields, (err, result) => {
-        if (err) res.json(err.message);
-        res.json(result);
-    });
-}));
-
-
-//Find user based on the username or email;
-router.get('/users/:username', verifier, asyncmw(async (req, res, next) => {
-    if (req.params.username) {
-        await userModel.findOne(
-            { $or: [{ email: req.params.username }, { username: req.params.username }] }, //Extract email from req.params
-            excludeFields, (err, fin) => {
-                if (!fin) return res.status(404).send({ "failure": "User not exists." });
-                return res.json(fin);
-            });
-    }
-}));
-
-//Delete user based on username or email
-router.delete('/users/:username', verifier, asyncmw(async (req, res) => {
-    await userModel.deleteOne({ $or: [{ username: req.params.username }, { email: req.params.username }] }, (err, user) => {
-        if (!err) {
-            if (user.n == 0 && user.ok == 1 && user.deletedCount == 0) {
-                return res.status(404).send({ message: 'No such user.' });
-            }
-            else {
-                return res.status(200).send({ message: 'Success' });
-            }
-        }
-        else
-            res.status(500).send('Internal Error');
-    });
-}));
-
+router.get('/users', [verifier, userCtrl.fetchUsers], asyncmw(async (req, res, next) => { }));
+router.get('/users/:username', [verifier, userCtrl.fetchOneUser], asyncmw(async (req, res, next) => { }));
+router.delete('/users/:username', [verifier, userCtrl.removeOneUser], asyncmw(async (req, res) => { }));
 
 module.exports = router;
