3802816c9619b723cdc4635df7c4ec00704c28df
knoxgon <volkan.guven@hotmail.se>
2019-08-15 16:22:00 +0200

    Refactorized login route

--- routes/login.js
+++ routes/login.js
@@ -1,53 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const jwtToken = require('jsonwebtoken');
-
-const userModel = require('../models/user-model');
-const secretKey = require('../config').secret.key;
-
 const asyncmw = require('../utils/async-middleware');
+const login_ctrl = require('../controllers/login-controller')
 
-const bcrypt = require('bcrypt');
-
-/*
-{
-        "firstName": "Billy",
-        "lastName": "Jones",
-        "email": "neutro_patrol@ssu.menningrad.co.uk",
-        "username": "aritros200x",
-        "password": "deimyaloaritmet4fa7@"
-}
-
-*/
-
-router.post('/login', asyncmw(async (req, res) => {
-    let username;
-
-    if (req.body.username != null) {
-        username = await userModel
-            .findOne(
-                {
-                    $or: [
-                        { username: req.body.username },
-                        { email: req.body.username }
-                    ]
-                }
-            )
-            .exec();
-        if (!username) {
-            return res.status(400).send({ message: "No such user" })
-        }
-    }
-    if (!bcrypt.compareSync(req.body.password, username.password)) {
-        return res.status(400).send({ message: "Wrong password" })
-    }
-    //Generate and send token within successful logon
-    const token = jwtToken.sign(
-        { user: username.username, isAdmin: username.isAdmin },
-        secretKey,
-        { expiresIn: '1d' });
-
-    res.status(200).json({ token: token });
-}));
+router.post('/login', login_ctrl, asyncmw(async (req, res) => { }));
 
 module.exports = router;
