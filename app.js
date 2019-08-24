4a3668f9242ff1ec0053ad885325e64aa87ab0eb
knoxgon <volkan.guven@hotmail.se>
2019-08-24 22:31:39 +0200

    Added cross origin allowance

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 }));
 
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
