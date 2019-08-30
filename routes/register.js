8194ab200629d1ce98025a29f4f7ae5acc4acf82
knoxgon <volkan.guven@hotmail.se>
2019-08-30 19:47:46 +0200

    Reconfigured register route to accept different users at different route gateways; [/client, /company]

--- routes/register.js
+++ routes/register.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const userModel = require('../models/user-model');
+const clientModel = require('../models/client-model');
+const companyModel = require('../models/company-model');
+
 const usernameCheck = require('../utils/validator');
 
 const bcrypt = require('bcrypt');
@@ -10,33 +12,43 @@ const asyncmw = require('../utils/async-middleware');
 
 /*
     {
-        "firstName": "John",
-        "lastName": "Doe",
-        "email": "john_doe@hotmail.com",
-        "username": "jd1995",
-        "password": "#@589142fa9",
-        "coc": "client"
-    }
+        client: {
+          "firstName": "John",
+          "lastName": "Doe",
+          "email": "john_doe@hotmail.com",
+          "username": "jd1995",
+          "password": "#@589142fa9",
+          "address": "journal avenue 74 CA",
+          "birthdate": "1975-05-19"
+        }
+    },
     {
-        "firstName": "Uthas",
-        "lastName": "Wilson",
-        "email": "uw@gmail.com",
-        "username": "utwill",
-        "password": "%)kda753z7",
-        "coc": "company"
-    }
-    {
-        "firstName": "Billy",
-        "lastName": "Jones",
-        "email": "neutro_patrol@ssu.menningrad.co.uk",
-        "username": "aritros200x",
-        "password": "deimyaloaritmet4fa7@",
-        "coc": "client"
+        company:
+        {
+          "corporateIdentityNumber": "556342-7391",
+          "companyName": "Ängelsbolag AB",
+          "address": "Björnkullavägen 75 Huddinge 14379",
+          "billingAddress": "Gamla Stan 12b 17452",
+          "contactFirstName": "Adam",
+          "contactLastName": "Eva",
+          "companyEmail": "angels@allabolag.se",
+          "password": "weArenotheretoGetAnything58129"
+        },
+        {
+          "corporateIdentityNumber": "581-5871-6",
+          "companyName": "Bolärnas Verkstad",
+          "address": "Sjödalsvägen 923 16922 Skogås",
+          "billingAddress": "Sjödalsvägen 923 16922 Skogås",
+          "contactFirstName": "Heidi",
+          "contactLastName": "Twinkler",
+          "companyEmail": "admin.office@bo.verkstad.com",
+          "password": "298@8FAUHJ4Tohrq1"
+        }
     }
 */
 
-//Create user based on Joi Validator
-router.post('/register', asyncmw(async (req, res, next) => {
+//Create client based on Joi Validator
+router.post('/register/client', asyncmw(async (req, res, next) => {
     //Validate username
     let result = usernameCheck.validateUsername(req.body.username);
     //Validate password
@@ -45,16 +57,16 @@ router.post('/register', asyncmw(async (req, res, next) => {
     }
     if (result) {
         req.body.password = bcrypt.hashSync(req.body.password, 10);
-        let user = new userModel(req.body);
+        let client = new clientModel(req.body);
         //Validate credentials
-        let userResult = user.joiValidate(req.body);
-        if (userResult[0]) {
-            await user.save().then(item => {
+        let clientResult = client.joiValidate(req.body);
+        if (clientResult[0]) {
+            await client.save().then(_ => {
                 res.sendStatus(201);
             }).catch((err) => res.status(400).send(err.errmsg));
         } else {
             //Send error list
-            let arrs = userResult[1].message.split('. ');
+            let arrs = clientResult[1].message.split('. ');
             let newList = [];
             for (var i = 0; i < arrs.length; i++) {
                 newList.push(JSON.stringify(
@@ -65,6 +77,33 @@ router.post('/register', asyncmw(async (req, res, next) => {
         }
     } else {
         res.status(400).send({ message: 'username cannot contain special characters' });
+    }
+}));
+
+//Create company based on Joi Validator
+router.post('/register/company', asyncmw(async (req, res, next) => {
+    //Validate password
+    if (req.body.password.length < 8) {
+        return res.status(400).send({ message: 'Password length must be 8 or more characters' });
+    }
+    req.body.password = bcrypt.hashSync(req.body.password, 10);
+    let company = new companyModel(req.body);
+    //Validate credentials
+    let companyResult = company.joiValidate(req.body);
+    if (companyResult[0]) {
+        await company.save().then(_ => {
+            res.sendStatus(201);
+        }).catch((err) => res.status(400).send(err.errmsg));
+    } else {
+        //Send error list
+        let arrs = companyResult[1].message.split('. ');
+        let newList = [];
+        for (var i = 0; i < arrs.length; i++) {
+            newList.push(JSON.stringify(
+                { errorMessage: arrs[i] })
+                .replace(/[\\'"]+/g, ''));
+        }
+        res.status(400).send(newList);
     }
 }));
 
